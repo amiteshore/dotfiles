6b5283d356aa2e4926ac17e4c1518d7113a716b2
amiteshore <amiteshore@gmail.com>
2019-10-25 19:05:20 +0530

    Add docker installation script

--- install.sh
+++ install.sh
@@ -57,3 +57,16 @@
 # wget -P ~/Downloads https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
 # tar -xvf ~/Downloads/go1.12.7.linux-amd64.tar.gz -C ~/Downloads
 # mv ~/Downloads/go /usr/local
+
+# Docker
+
+# apt update
+# apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+# add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
+# apt update
+# apt-cache policy docker-ce
+# apt install docker-ce
