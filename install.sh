f1bbc1952f97caef05162f424602d3a6b1383906
amiteshore <amiteshore@gmail.com>
2019-10-27 17:23:04 +0530

    Add nvm latest version

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 # Node.js
 
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
