9ad645fea12d63a6e99f4af722811fe0ad94cbe0
amiteshore <amiteshore@gmail.com>
2020-03-22 22:08:19 +0530

    Twekas

--- install.sh
+++ install.sh
@@ -8,12 +8,11 @@ sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
 
 # Node.js
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
-nvm use --lts
 
 
 # Deno
@@ -26,7 +25,7 @@ export PATH="$DENO_INSTALL/bin:$PATH"
 # MongoDB
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update
 sudo apt install mongodb-org --yes
 
