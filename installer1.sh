a0054db44121a967eb9351ee615857fdeb0f2efd
amiteshore <amiteshore@gmail.com>
2021-03-29 13:30:42 +0530

    Tweaks

--- installer1.sh
+++ installer1.sh
@@ -13,9 +13,8 @@ pacman -S wget
 pacman -S curl
 pacman -S ufw
 pacman -S tree
-pacman -S unzip
-pacman -S tar
 pacman -S htop
+pacman -S rsync
 pacman -S neofetch
 pacman -S feh
 pacman -S pulseaudio
@@ -23,15 +22,15 @@ pacman -S rofi
 pacman -S openssh
 pacman -S polkit
 pacman -S aria2
-pacman -S zsh-autosuggestions
-pacman -S zsh-syntax-highlighting
-pacman -S zsh-history-substring-search
-pacman -S zsh-completions
 pacman -S bat
 pacman -S exa
 pacman -S ranger
 pacman -S thunar
 pacman -S numlockx
+pacman -S zsh-autosuggestions           # zsh plugin
+pacman -S zsh-syntax-highlighting       # zsh plugin
+pacman -S zsh-history-substring-search  # zsh plugin
+pacman -S zsh-completions               # zsh plugin
 
 # =============== Development =============== #
 pacman -S docker
