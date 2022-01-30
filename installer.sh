4f560cf503428836ef56f9320009a772826f3f1a
Amit Eshore <amiteshore@gmail.com>
2022-01-30 21:44:24 +0530

    Modified list

--- installer.sh
+++ installer.sh
@@ -2,66 +2,68 @@
 
 # Install packages from the official arch repo, AUR and other sources
 
-# =============== Utils =============== #
-sudo pacman -S xorg           # group
-sudo pacman -S base-devel     # group
-sudo pacman -S i3             # group
-sudo pacman -S xorg-xinit
-sudo pacman -S firefox-developer-edition
-sudo pacman -S discord
-sudo pacman -S alacritty
-sudo pacman -S starship
-sudo pacman -S ripgrep
-sudo pacman -S fd
-sudo pacman -S bat
-sudo pacman -S exa
-sudo pacman -S fzf
-sudo pacman -S tmux
-sudo pacman -S wget
-sudo pacman -S curl
-sudo pacman -S ufw
-sudo pacman -S tree
-sudo pacman -S rsync
-sudo pacman -S openssh
-sudo pacman -S htop
-sudo pacman -S neofetch
-sudo pacman -S feh
-sudo pacman -S rofi
-sudo pacman -S aria2
-sudo pacman -S thunar
-sudo pacman -S xsel
-sudo pacman -S numlockx
-sudo pacman -S zsh-autosuggestions           # zsh plugin
-sudo pacman -S zsh-syntax-highlighting       # zsh plugin
-sudo pacman -S zsh-history-substring-search  # zsh plugin
-sudo pacman -S zsh-completions               # zsh plugin
-sudo pacman -S gnome-themes-extra
-sudo pacman -S python-pynvim
+packages=(
+  'xorg'              # group
+  'base-devel'        # group
+  'i3'                # group
+  'firefox-developer-edition'
+  'discord'
+  'alacritty'
+  'kitty'
+  'starship'
+  'ripgrep'
+  'fd'
+  'bat'
+  'exa'
+  'fzf'
+  'tmux'
+  'wget'
+  'curl'
+  'ufw'
+  'tree'
+  'htop'
+  'neofetch'
+  'rsync'
+  'openssh'
+  'aria2'
+  'thunar'
+  'rofi'
+  # zsh plugins
+  'zsh-autosuggestions'
+  'zsh-syntax-highlighting'
+  'zsh-history-substring-search'
+  'zsh-completions'
+  # bluetooth and audio
+  'pulseaudio'
+  'pulseaudio-alsa'
+  'pulseaudio-bluetooth'
+  'bluez'
+  'bluez-utils'
+  'bluez-hid2hci'
+  # development
+  'mariadb'
+  'redis'
+  'docker'
+  'httpie'
+  'nginx'
+  'clang'
+  'python'
+  'python-pip'
+  # fonts
+  'ttf-fira-code'
+  'ttf-font-awesome'
+  'noto-fonts'
+  'noto-fonts-emoji'
+  # helpers
+  'feh'
+  'xorg-xinit'
+  'xsel'
+  'numlockx'
+  'python-pynvim'
+  'gnome-themes-extra'
+)
 
-# =============== Bluetooth and audio =============== #
-sudo pacman -S pulseaudio
-sudo pacman -S pulseaudio-alsa
-sudo pacman -S pulseaudio-bluetooth
-sudo pacman -S bluez
-sudo pacman -S bluez-utils
-sudo pacman -S bluez-hid2hci
-
-# =============== Development =============== #
-sudo pacman -S mariadb
-sudo pacman -S redis
-sudo pacman -S docker
-sudo pacman -S httpie
-sudo pacman -S nginx
-sudo pacman -S clang
-sudo pacman -S python
-sudo pacman -S python-pip
-#sudo pacman -S code
-
-# =============== Fonts =============== #
-sudo pacman -S ttf-fira-code
-sudo pacman -S ttf-font-awesome
-sudo pacman -S noto-fonts
-sudo pacman -S noto-fonts-emoji
+sudo pacman -S ${packages[@]}
 
 # AUR PACKAGES #
 
@@ -91,7 +93,7 @@ echo "\n" >> "$HOME/.zshrc"
 echo -n "# Load nvm script" >> "$HOME/.zshrc"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.zshrc
-nvm install node
+nvm install --lts
 
 # =============== npm global pagkages =============== #
 npm install -g typescript ts-node sass ngrok redis-commander nodemon live-server npm-check-updates
@@ -99,9 +101,3 @@ npm install -g typescript ts-node sass ngrok redis-commander nodemon live-server
 # =============== vim-plug =============== #
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# vscode extensions
-#code --install-extension PKief.material-icon-theme
-#code --install-extension sdras.night-owl
-#code --install-extension vscodevim.vim
-#code --install-extension esbenp.prettier-vscode
