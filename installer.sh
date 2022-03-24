6729c2749be3a3a4fe1ea808cbbc2da96e667f3d
Amit Eshore <amiteshore@gmail.com>
2022-03-24 11:57:58 +0530

    Add tmux-resurrect

--- installer.sh
+++ installer.sh
@@ -126,3 +126,6 @@ npm install -g ${npm_packages[@]}
 # =============== vim-plug =============== #
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# =============== tmux plugins =============== #
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
