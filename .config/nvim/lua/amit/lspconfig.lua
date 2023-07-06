7edc6a8650b3d6db7c5c009ec33bc1d95a9bcdda
Amit Eshore <amiteshore@gmail.com>
2023-07-06 14:05:34 +0530

    Update

--- .config/nvim/lua/amit/lspconfig.lua
+++ .config/nvim/lua/amit/lspconfig.lua
@@ -1,6 +1,6 @@
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require("lspconfig")["sumneko_lua"].setup({
+require("lspconfig")["lua_ls"].setup({
 	capabilities = capabilities,
 	on_attach = function(client)
 		if client.name == "sumneko_lua" then
@@ -23,6 +23,7 @@ require("lspconfig")["sumneko_lua"].setup({
 			},
 			workspace = {
 				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
 			},
 		},
 	},
--- .config/nvim/lua/amit/null_ls.lua
+++ .config/nvim/lua/amit/null_ls.lua
@@ -10,7 +10,8 @@ require("null-ls").setup({
 				buffer = bufnr,
 				callback = function()
 					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-					vim.lsp.buf.formatting_sync()
+					-- vim.lsp.buf.formatting_sync()
+					vim.lsp.buf.format({ bufnr = bufnr })
 				end,
 			})
 		end
